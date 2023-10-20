d657bcf54e605e279af69c7beb1ecce46747fb9e
DanHerzberg <dan03herzberg@gmail.com>
2023-10-20 14:02:36 -0600

    Organizacion paginas

---
+++ Pages/PaginaRegistro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontCrossyTec.Pages
+{
+    public class PaginaRegistroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
