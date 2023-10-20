69be12c08c47ba5c4e4ee4cf1ef753ff589b5d38
Javier <113366019+Javidr2243@users.noreply.github.com>
2023-10-20 17:47:52 -0600

    Se crea la pagina de login
    Se crea la pagina de login, el boton de iniciar sesion te redirige a la pantalla de registro

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace FrontCrossyTec.Pages
 {
@@ -12,9 +13,32 @@ namespace FrontCrossyTec.Pages
             _logger = logger;
         }
 
+        [BindProperty]
+        public string LoginEmail { get; set; }
+
+        [BindProperty]
+        public string LoginPassword { get; set; }
+
+        public string ErrorMessage { get; set; } = string.Empty;
+
         public void OnGet()
         {
+            
+        }
 
+        public IActionResult OnPost()
+        {
+            if (LoginEmail == "pepito@hotmail.com" && LoginPassword == "123")
+            {
+                // Redirige a la página de registro si las credenciales son correctas.
+                return RedirectToPage("PaginaRegistro");
+            }
+            else
+            {
+                // Muestra un mensaje de error si las credenciales son incorrectas.
+                ErrorMessage = "Credenciales inválidas";
+                return Page();
+            }
         }
     }
 }
