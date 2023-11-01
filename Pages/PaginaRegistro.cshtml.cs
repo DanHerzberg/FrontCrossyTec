b131a2a0610e73a4f4a0c894b47fdd06e38a08c2
Javier <113366019+Javidr2243@users.noreply.github.com>
2023-11-01 12:48:14 -0600

    Cambio a registros
    se agrega cambios a la pagina de registros y la capacidad de mandar el formulario al backend de la pagina

--- Pages/PaginaRegistro.cshtml.cs
+++ Pages/PaginaRegistro.cshtml.cs
@@ -5,8 +5,15 @@ namespace FrontCrossyTec.Pages
 {
     public class PaginaRegistroModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnPost()
         {
+            // Aquí procesas los datos del formulario
+            // Por ejemplo, puedes recuperar los valores así:
+            
+            // ...y así sucesivamente para otros campos
+
+            // Luego, rediriges a la página que desees. Por ejemplo, a una página de éxito:
+            return RedirectToPage("PaginaMenu");
         }
     }
 }
