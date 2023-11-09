d65882ad84941f3295a3179ab7b60a192c9ea5d5
Javier <113366019+Javidr2243@users.noreply.github.com>
2023-11-08 22:48:08 -0600

    Se agrega pagina de scoreboard
    La pagina de scoreboard tiene datos estaticos actualmente, hace falta agregarle un modelo llamado jugadores y en el controlador de la pagina de scoreboard la logica para obtener desde la BD los datos de los jugadores

---
+++ Pages/PaginaScoreboard.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontCrossyTec.Pages
+{
+	public class PaginaScoreboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
