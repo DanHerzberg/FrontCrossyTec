306dc753bb209fa406a2d7da6aa6233e5eeba8ce
DanHerzberg <dan03herzberg@gmail.com>
2023-11-10 20:14:06 -0600

    apiservices mdificado actual

--- Model/ApiService.cs
+++ Model/ApiService.cs
@@ -4,6 +4,7 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json; // Si estás utilizando Newtonsoft.Json para la deserialización
+using System.Text;
 
 namespace FrontCrossyTec.Model
 {
@@ -64,6 +65,32 @@ namespace FrontCrossyTec.Model
                 return null;
             }
         }
+
+        public async Task<HttpResponseMessage> RegisterAsync(RegistroUsuario registroUsuario)
+        {
+            try
+            {
+                string jsonRegistro = JsonConvert.SerializeObject(registroUsuario);
+                var content = new StringContent(jsonRegistro, Encoding.UTF8, "application/json");
+                var response = await _httpClient.PostAsync("api/RegisterUser", content);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<HttpResponseMessage> LoginAsync(string email, string password)
+        {
+            var loginInfo = new { Email = email, Password = password };
+            string jsonLogin = JsonConvert.SerializeObject(loginInfo);
+            var content = new StringContent(jsonLogin, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/Login", content);
+
+            return response;
+        }
     }
 
     public class ChestDto
@@ -78,6 +105,20 @@ namespace FrontCrossyTec.Model
         public string email { get; set; }
         public string password { get; set; }
 
+    }
+
+    public class RegistroUsuario
+    {
+        public string name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string gamertag { get; set; }
+        public string password { get; set; }
+        public DateTime birth_date { get; set; }
+        public string gender { get; set; }
+        public string state { get; set; }
+        public int coins { get; set; } = 0;
+        public string rol { get; set; } = "Player";
     }
 
 }
