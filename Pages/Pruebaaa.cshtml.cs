7a8d2bbd367aa9337ca2ef42f4913b4f2d67337a
Javier <113366019+Javidr2243@users.noreply.github.com>
2023-11-16 13:46:27 -0600

    Se guardan datos del usuario en la sesion

---
+++ Pages/Pruebaaa.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace FrontCrossyTec.Pages
+{
+    public class PruebaaaModel : PageModel
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public string UserId { get; private set; }
+        public string UserName { get; private set; }
+        // Agrega más propiedades según los datos de sesión que desees mostrar
+
+        public PruebaaaModel(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public void OnGet()
+        {
+            UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId");
+            UserName = _httpContextAccessor.HttpContext.Session.GetString("UserName");
+            // Obten más datos de sesión según tus necesidades
+        }
+    }
+}
