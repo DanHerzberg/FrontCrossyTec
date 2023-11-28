4eeb42ce8e21d60da31a3c9e5bf4c6fa2e99fcfa
Javier <113366019+Javidr2243@users.noreply.github.com>
2023-11-27 18:16:56 -0600

    se crea pagina perfil

---
+++ Pages/PaginaPerfil.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontCrossyTec.Pages
+{
+	public class PaginaPerfilModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
